ff0e6ae21362b6e94965977a5e6d110005d0373d
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2022-07-19 12:01:30 +0800

    update input

--- project4.py
+++ project4.py
@@ -4,3 +4,8 @@ print("Hello everyone! Testing my Github.")
 print("This code is written by Waiee Zainol.")
 
 print("Test third commmit.")
+
+#Streamlit
+
+print("----- Price Analysis -----")
+userInput = input("What type of company price you want to see?: ")
